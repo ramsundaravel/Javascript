76aff6ee3843665b685637665bc9e7c0a92d3d52
Varsha Ram kumar <varsharamkumar@Varshas-MacBook-Pro.local>
2018-08-30 11:27:48 -0400

    note from 08/30

--- jsbasics/index.js
+++ jsbasics/index.js
@@ -20,5 +20,29 @@ console.log(selectedcolor.length);
 function greet(var1){
     console.log("hello " + var1)
 }
-
 greet('Ram')
+
+// arrary
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(typeof(fruits))
+
+
+var numbers1 = [45, 4, 9, 16, 25];
+
+// call method example
+let person1 = {
+    fname : "Ram",
+    lname : "Sundaravel",
+    getname : function(){
+        console.log(this.fname + " "+ this.lname)
+    }
+}
+
+let person2 = {
+    fname : "Varsha",
+    lname : "Ram"
+}
+
+person1.getname()
+person1.getname.call(person2) // call method used to 
+// send other object and this function will refer to passing object
