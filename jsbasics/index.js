b2f1bbc86ae3edcc9fed79f52b5407764372dae2
Varsha Ram kumar <varsharamkumar@Varshas-MacBook-Pro.local>
2018-08-30 09:22:46 -0400

    updates

---
+++ jsbasics/index.js
@@ -0,0 +1,24 @@
+console.log('Hello World1');
+let name = 'Ram';
+console.log(name);
+
+let person = {
+    name : "Ram",
+    age : 30
+}
+
+console.log(person);
+console.log(person.name);
+
+// array
+let selectedcolor = ['red','blue'];
+console.log(selectedcolor);
+console.log(selectedcolor.length);
+
+
+//function
+function greet(var1){
+    console.log("hello " + var1)
+}
+
+greet('Ram')
